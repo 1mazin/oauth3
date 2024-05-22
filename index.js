d958c99a5d31131813bca7d5814b519058a92dc6
Mehnaz <mehnazuluwar@gmail.com>
2024-05-22 13:49:31 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const axios = require('axios');
+const app = express();
 require('dotenv').config();
 
-const app = express();
-const port = 3000;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
@@ -36,6 +35,4 @@ app.get('/callback', async (req, res) => {
     res.send(`<h1>Hello, ${userData.login}</h1><img src="${userData.avatar_url}" alt="avatar" width="100">`);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = app;
